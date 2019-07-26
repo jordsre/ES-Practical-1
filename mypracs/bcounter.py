ff06ce5083856aada094e8153b3471ad789d0fd7
jordsre <jordsre@gmail.com>
2019-07-26 16:26:30 +0100

    Makes a single LED blink every 1s

--- mypracs/bcounter.py
+++ mypracs/bcounter.py
@@ -11,16 +11,28 @@ Date: 22/07/2019
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+
+
+
 
 # Logic that you write
 def main():
-    print("write your logic here")
 
+	GPIO.output(7,GPIO.HIGH)
+	sleep(1)
+	GPIO.output(7,GPIO.LOW)
+	sleep(1)
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
+	
         while True:
             main()
     except KeyboardInterrupt:
