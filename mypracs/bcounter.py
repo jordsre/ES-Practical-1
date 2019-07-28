58de6420136d58e3211f73ecc17d7a95cb8ef2d2
jordsre <jordsre@gmail.com>
2019-07-28 14:26:19 +0100

    Toggles the state of a single LED

--- mypracs/bcounter.py
+++ mypracs/bcounter.py
@@ -13,33 +13,42 @@ Date: 22/07/2019
 import RPi.GPIO as GPIO
 from time import sleep
 
+# pin numbers
+button1 = 11
+LED1 = 7
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT)
 
+GPIO.setup(LED1,GPIO.OUT) # LED 1
+GPIO.setup(button1,GPIO.IN, pull_up_down=GPIO.PUD_UP) # pushbutton
+ 
 
-
+# GPIO.add_event_detect(BTN_PIN, GPIO.FALLING, callback=callback_method(),bouncetime=300)
 
 # Logic that you write
 def main():
 
-	GPIO.output(7,GPIO.HIGH)
-	sleep(1)
-	GPIO.output(7,GPIO.LOW)
-	sleep(1)
-
-# Only run the functions if 
+	GPIO.output(LED1,GPIO.input(button1))
+	sleep(.1)
+# Only run the functions if
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
-	
-        while True:
+
+       while True:
             main()
+
     except KeyboardInterrupt:
-        print("Exiting gracefully")
+    	print("Exiting gracefully")
         # Turn off your GPIOs here
-        GPIO.cleanup()
+        GPIO.output(LED1,False)
+	GPIO.cleanup()
+
     except e:
         GPIO.cleanup()
         print("Some other error occurred")
         print(e.message)
+
+
+
